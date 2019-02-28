b51c921d1f13721bcf50a96b057401e8d7c62aff
Marrucha <mmatczuk@g.pl>
2019-02-28 01:03:38 +0100

    W/O Optional

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -8,18 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class TasksApplicationTests {
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class TasksApplicationTests {
 
-	@Autowired
-	TaskRepository taskRep;
-    @Test
-	public void contextLoads() {
+	//@Autowired
+	//TaskRepository taskRep;
+    //@Test
+	//public void contextLoads() {
 
 /*    	Task newTask = new Task(2L,"test2","test3");
     	taskRep.save(newTask);*/
-	}
+//	}
 
-}
+//}
 
